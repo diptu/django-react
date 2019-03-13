faef8861427cedeab5b5b1f7e7fae1de036a49ef
Nazmul Alam Diptu <nazmul.diptu@northsouth.edu>
2019-03-14 00:05:06 +0600

    react-setup

---
+++ leads/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Lead(models.Model):
+    name        =   models.CharField(max_length=100)
+    email       =   models.EmailField(max_length=100,unique=True)
+    message     =   models.TextField(blank=True)
+    created_at  =   models.DateTimeField(auto_now_add=True)
+    updated_at  =   models.DateTimeField(auto_now=True)
